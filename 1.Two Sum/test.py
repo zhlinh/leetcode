bfdb6309451181b0e19bdf4b5738773695bc8b81
zhlinh <zhlinhng@gmail.com>
2016-01-04 03:02:05 +0000

    add 1.Two Sum

---
+++ 1.Two Sum/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+numbers = [2, 7, 11, 15]
+target = 17
+sol = Solution()
+index = sol.twoSum(numbers, target)
+print("index1=%d, index2=%d" % index)
