5ef8f5894ded6c5fd7916f09af6d61c1c6c8bd66
zhlinh <zhlinhng@gmail.com>
2016-01-05 14:02:28 +0000

    better solution

--- 3.Longest Substring Without Repeating Characters/README.md
+++ 3.Longest Substring Without Repeating Characters/README.md
@@ -17,7 +17,7 @@ ghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@
 ...省略300多行(无换行符)，31655个字符...”
 ```
 
-后来提交了一版，也是O(n^2)，但这次的第二个循环只用来更新dict。
+后来提交了一版，也是O(n^2)，但这次的第二个循环只用来重新建立dict。
 
 通过了，但效率还是太低。
 
@@ -27,3 +27,20 @@ submit的结果为:
 Status: Accepted
 Runtime: 432 ms
 ```
+
+然后发现自己真的智商捉急了，根本不需要再浪费一个循环来更新dict。
+
+而只需再加一个判断d[s[i]] >= start即可。
+
+也就是说如果d[s[i]] < start的话就相当于不属于当前dict的，而不需要重新建立dict。
+
+此时的时间复杂度为O[n]。
+
+submit的结果为:
+```
+981 / 981 test cases passed.
+Status: Accepted
+Runtime: 100 ms
+```
+
+
--- 3.Longest Substring Without Repeating Characters/solution.py
+++ 3.Longest Substring Without Repeating Characters/solution.py
@@ -26,18 +26,14 @@ class Solution(object):
         """
         d = {}
         max = 0
-        substr = None
+        #substr = None
         start = 0
         for i in range(len(s)):
-            if s[i] in d:
+            if s[i] in d and d[s[i]] >= start:
                 start = d[s[i]] + 1
-                d = {}
-                for j in range(start,i):
-                    d[s[j]] = j
             d[s[i]] = i
             if (i - start + 1) > max:
                 max = i - start + 1
                 #若需要求解substr
                 #substr = s[start:i+1]
         return max
-
