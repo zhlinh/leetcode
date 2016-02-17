f477eaaaff556d985303108dcc84e46b8f95f1af
zhlinh <zhlinhng@gmail.com>
2016-02-17 01:24:43 +0000

    add 35.Search Insert Position

---
+++ 35.Search Insert Position/README.md
@@ -0,0 +1,10 @@
+第一版用的二分法，即bisect_left的实现。
+
+时间复杂度为O(log(n))。
+
+submit的结果为:
+```
+62 / 62 test cases passed.
+Status: Accepted
+Runtime: 48 ms
+```
---
+++ 35.Search Insert Position/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+inums = [1, 3, 5, 6]
+target = 0
+sol = Solution()
+res = sol.searchInsert(inums, target)
+print(res)
