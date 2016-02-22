54409ec7d29c8879f90559a0fa7642bf032636db
zhlinh <zhlinhng@gmail.com>
2016-02-22 13:00:23 +0000

    concise code

--- 54.Spiral Matrix/README.md
+++ 54.Spiral Matrix/README.md
@@ -2,9 +2,29 @@
 
 值得注意的最好是先移动坐标，再赋值，否则逻辑会很繁琐。
 
+时间复杂度为O(m * n)
+
 submit的结果为:
 ```
 22 / 22 test cases passed.
 Status: Accepted
 Runtime: 52 ms
 ```
+
+第二版用了一个dirs和一个step来保存方向及要走的步数，这样就可以很好地精简代码。
+
+    dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    step = [nc, nr]
+
+时间复杂度为O(m * n)
+
+submit的结果为:
+```
+22 / 22 test cases passed.
+Status: Accepted
+Runtime: 44 ms
+```
+
+还想到一个不断pop的思路，一行一列地往外pop。
+
+不过时间复杂度可能会大于O(m * n)，所以就算了。
--- 54.Spiral Matrix/solution.py
+++ 54.Spiral Matrix/solution.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 '''
 *****************************************
-Author:         zhsteps_inh
-Email:          zhsteps_inhng@gmail.com
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
 Version:        0.0.1
 Created Time:   2016-02-22
 Last_modify:    2016-02-22
@@ -30,29 +30,21 @@ class Solution(object):
         :type matrix: steps_ist[steps_ist[int]]
         :rtype: steps_ist[int]
         """
-        m = len(matrix)
-        if m == 0:
-            return []
-        n = len(matrix[0])
-        if n == 0:
-            return []
-        res = [0 for _ in range(m*n)]
-        i, j, k = 0, -1, 0
-        dir_i, dir_j = 1, 1
-        steps_i, steps_j = m, n
-        while k < m * n:
-            if dir_i * dir_j == 1:
-                for _ in range(steps_j):
-                    j += dir_j
-                    res[k] = matrix[i][j]
-                    k += 1
-                dir_j = -dir_j
-                steps_i -= 1
-            else:
-                for _ in range(steps_i):
-                    i += dir_i
-                    res[k] = matrix[i][j]
-                    k += 1
-                dir_i = -dir_i
-                steps_j -= 1
+        res = []
+        nr = len(matrix)
+        if nr == 0:
+            return res
+        nc = len(matrix[0])
+        if nc == 0:
+            return res
+        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        step = [nc, nr]
+        iDir, ir, ic = 0, 0, -1
+        while step[iDir%2] > 0 :
+            for _ in range(step[iDir%2]):
+                ir += dirs[iDir][0]
+                ic += dirs[iDir][1]
+                res.append(matrix[ir][ic])
+            iDir = (iDir + 1) % 4
+            step[iDir%2] -= 1
         return res
