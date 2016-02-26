357882664364a041e4f185b41f20999b662e5651
zhlinh <zhlinhng@gmail.com>
2016-02-26 10:32:12 +0000

    Newton's method solution

--- 69.Sqrt(x)/README.md
+++ 69.Sqrt(x)/README.md
@@ -8,3 +8,16 @@ submit的结果为:
 Status: Accepted
 Runtime: 64 ms
 ```
+
+第二版用了Newton Method(牛顿法)来解决。
+
+解x^2 - n = 0，则有
+
+x[k+1] = (1/2) * (x[k] + n/x[k])
+
+submit的结果为:
+```
+1017 / 1017 test cases passed.
+Status: Accepted
+Runtime: 68 ms
+```
--- 69.Sqrt(x)/solution.py
+++ 69.Sqrt(x)/solution.py
@@ -21,11 +21,7 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        i, j = 0, x + 1
-        while i < j:
-            mid = (i + j) // 2
-            if mid * mid > x:
-                j = mid
-            else:
-                i = mid + 1
-        return i - 1
+        n = x
+        while n * n > x:
+            n = (n + x // n) // 2
+        return n
