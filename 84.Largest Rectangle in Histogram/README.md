4db868d8011a1e6dd04ea7041ca7a55f53a84616
zhlinh <zhlinhng@gmail.com>
2016-03-01 11:10:29 +0000

    add 84.Largest Rectangle in Histogram

---
+++ 84.Largest Rectangle in Histogram/README.md
@@ -0,0 +1,8 @@
+第一版用了一个stack，记录的是高度从小到大排列的元素的下标。
+
+submit的结果为:
+```
+94 / 94 test cases passed.
+Status: Accepted
+Runtime: 80 ms
+```
---
+++ 84.Largest Rectangle in Histogram/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+heights = [2, 1, 5, 6, 2, 3]
+sol = Solution()
+res = sol.largestRectangleArea(heights)
+print(res)
