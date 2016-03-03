500e83636995678aa94778affa0d56633fbe79ec
zhlinh <zhlinhng@gmail.com>
2016-03-03 09:29:27 +0000

    add 93.Restore IP Addresses

---
+++ 93.Restore IP Addresses/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+#  inpt = "25525511135"
+#  inpt = "0000"
+inpt = "010010"
+sol = Solution()
+res = sol.restoreIpAddresses(inpt)
+print(res)
