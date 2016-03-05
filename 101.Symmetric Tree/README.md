1656b0f7b59b1d1902849b513f0e24c9efd0dd18
zhlinh <zhlinhng@gmail.com>
2016-03-05 13:10:23 +0000

    add 101.Symmetric Tree

---
+++ 101.Symmetric Tree/README.md
@@ -0,0 +1,8 @@
+第一版用了递归前序遍历，分为两个部分作镜面比较。
+
+submit的结果为:
+```
+192 / 192 test cases passed.
+Status: Accepted
+Runtime: 60 ms
+```
---
+++ 101.Symmetric Tree/solution.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-03-05
+Last_modify:    2016-03-05
+******************************************
+'''
+
+'''
+Given a binary tree,
+check whether it is a mirror of itself
+(ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.helper(root.left, root.right)
+
+    def helper(self, l, r):
+        if not l or not r:
+            return l== r
+        if l.val == r.val:
+            return self.helper(l.left, r.right) and self.helper(l.right, r.left)
+        return False
