e859e9b38ca5c4d43bb119e069296d443cc30e4a
zhlinh <zhlinhng@gmail.com>
2016-03-06 01:46:56 +0000

    add 103.Binary Tree Zigzag Level Order Traversal

---
+++ 103.Binary Tree Zigzag Level Order Traversal/README.md
@@ -0,0 +1,12 @@
+第一版用了BFS。
+
+每一次的queue里都加的是本行的元素。
+
+就是间隔地要把level倒序，然后再添加到levels结果数组中。
+
+submit的结果为:
+```
+33 / 33 test cases passed.
+Status: Accepted
+Runtime: 52 ms
+```
---
+++ 103.Binary Tree Zigzag Level Order Traversal/solution.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-03-06
+Last_modify:    2016-03-06
+******************************************
+'''
+
+'''
+Given a binary tree,
+return the zigzag level order traversal of its nodes' values.
+(ie, from left to right, then right to left for the next
+level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        q = [root]
+        levels = []
+        order = 1
+        while q:
+            level = []
+            n = len(q)
+            order = (order + 1) % 2
+            for i in range(n):
+                cur = q.pop(0)
+                level.append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            if order:
+                level = level[::-1]
+            levels.append(level)
+        return levels
