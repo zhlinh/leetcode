058b04cf53abe83678524c2b6593fced77b2ca3c
zhlinh <zhlinhng@gmail.com>
2016-03-19 08:15:49 +0000

    add 147.Insertion Sort List

---
+++ 147.Insertion Sort List/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+from solution import ListNode
+
+def mkListNode(nums):
+    """
+    :type: nums: List
+    :rtype: ListNode
+    """
+    head = lnp = ListNode(0)
+    for i in range(len(nums)):
+        lnp.next = ListNode(nums[i])
+        lnp = lnp.next
+    return head.next
+
+def lsListNode(head):
+    """
+    :type: head: ListNode
+    :rtype: List
+    """
+    lnp = head
+    rList = []
+    while lnp != None:
+        rList.append(lnp.val)
+        lnp = lnp.next
+    print(rList)
+
+inpt = mkListNode([3, 2, 0, -4])
+lsListNode(inpt)
+sol = Solution()
+res = sol.insertionSortList(inpt)
+lsListNode(res)
