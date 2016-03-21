6400a26d84a2781714e6733b0eee6661835d56e7
zhlinh <zhlinhng@gmail.com>
2016-03-21 13:22:52 +0000

    add 160.Intersection of Two Linked Lists

---
+++ 160.Intersection of Two Linked Lists/solution.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-03-21
+Last_modify:    2016-03-21
+******************************************
+'''
+
+'''
+Write a program to find the node at which
+the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+Credits:
+Special thanks to @stellari for adding this problem and creating all test cases.
+'''
+
+# Definition for singly-linked list.
+class ListNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+        h = headA
+        while h.next:
+            h = h.next
+        end = h
+        h.next = headB
+        fast = slow = h = headA
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                while slow:
+                    if h == slow:
+                        end.next = None
+                        return h.val
+                    slow = slow.next
+                    h = h.next
+        end.next = None
+        return None
