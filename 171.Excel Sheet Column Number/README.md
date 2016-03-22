150f1c0b0f13aebb1efbfb3914b470b8503ba172
zhlinh <zhlinhng@gmail.com>
2016-03-22 12:10:36 +0000

    add 171.Excel Sheet Column Number

---
+++ 171.Excel Sheet Column Number/README.md
@@ -0,0 +1,10 @@
+第一版就是26进制转10进制的思路。
+
+注意在循环里加1即可，因A对应的是1。
+
+submit的结果为:
+```
+1000 / 1000 test cases passed.
+Status: Accepted
+Runtime: 72 ms
+```
---
+++ 171.Excel Sheet Column Number/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+s = "AA"
+#  s = 'Z'
+sol = Solution()
+res = sol.titleToNumber(s)
+print(res)
