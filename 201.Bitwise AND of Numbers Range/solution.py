b8815306651a55e1199c03bcd197a225fef8cb1a
zhlinh <zhlinhng@gmail.com>
2016-03-24 10:31:35 +0000

    add 201.Bitwise AND of Numbers Range

---
+++ 201.Bitwise AND of Numbers Range/solution.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-03-24
+Last_modify:    2016-03-24
+******************************************
+'''
+
+'''
+Given a range [m, n] where 0 <= m <= n <= 2147483647,
+return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+'''
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 0:
+            return 0
+        length = n - m + 1
+        shift = 0
+        base = 1
+        while length > base:
+            base <<= 1
+            shift += 1
+        return m & n & (0xFFFFFFFF << shift)
---
+++ 201.Bitwise AND of Numbers Range/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+m, n = 3, 4
+sol = Solution()
+res = sol.rangeBitwiseAnd(m, n)
+print(res)
