fafb33b2a3ec0431a8dd810c959c54aa39149700
zhlinh <zhlinhng@gmail.com>
2016-03-24 12:13:52 +0000

    add 202.Happy Number

---
+++ 202.Happy Number/README.md
@@ -0,0 +1,10 @@
+第一版就是按照定义来解的。如果出现重复就退出循环，返回False。
+
+符合条件就返回True。
+
+submit的结果为:
+```
+400 / 400 test cases passed.
+Status: Accepted
+Runtime: 56 ms
+```
---
+++ 202.Happy Number/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+n = 19
+sol = Solution()
+res = sol.isHappy(n)
+print(res)
