8ad2e9d04a52886e9716f0ec7b5e409fb175f931
zhlinh <zhlinhng@gmail.com>
2016-03-24 04:21:08 +0000

    add 199.Binary Tree Right Side View

---
+++ 199.Binary Tree Right Side View/README.md
@@ -0,0 +1,8 @@
+很容易想到的就是BFS，然后将每层的最后一个节点的值放入res中。
+
+submit的结果为:
+```
+210 / 210 test cases passed.
+Status: Accepted
+Runtime: 58 ms
+```
---
+++ 199.Binary Tree Right Side View/solution.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-03-24
+Last_modify:    2016-03-24
+******************************************
+'''
+
+'''
+Given a binary tree, imagine yourself standing on the right side of it,
+return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        q = [root]
+        cur = None
+        res = []
+        while q:
+            n = len(q)
+            for i in range(n):
+                cur = q.pop(0)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            res.append(cur.val)
+        return res
