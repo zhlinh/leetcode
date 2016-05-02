d0bfa7f955c558666b4138a3d0e92a41009fe315
zhlinh <zhlinhng@gmail.com>
2016-05-02 09:29:24 +0000

    add 226.Invert Binary Tree

---
+++ 226.Invert Binary Tree/README.md
@@ -0,0 +1,8 @@
+很自然地想到BFS，然后就交换左右子树。
+
+submit的结果为:
+```
+68 / 68 test cases passed.
+Status: Accepted
+Runtime: 48 ms
+```
---
+++ 226.Invert Binary Tree/solution.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-05-02
+Last_modify:    2016-05-02
+******************************************
+'''
+
+'''
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew),
+but you can’t invert a binary tree on a whiteboard so fuck off.
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        q = [root]
+        while q:
+            cur = q.pop(0)
+            cur.left, cur.right = cur.right, cur.left
+            if cur.left:
+                q.append(cur.left)
+            if cur.right:
+                q.append(cur.right)
+        return root
