e18d6b7d14d595b5b368157f7218caccdd7f8094
zhlinh <zhlinhng@gmail.com>
2016-05-03 03:14:43 +0000

    add 230.Kth Smallest Element in a BST

---
+++ 230.Kth Smallest Element in a BST/README.md
@@ -0,0 +1,10 @@
+用了中序遍历，即根据二叉搜索树的特点，左 < 中 < 右得到第k个最小值。
+
+时间复杂度为O(k)。
+
+submit的结果为:
+```
+91 / 91 test cases passed.
+Status: Accepted
+Runtime: 87 ms
+```
---
+++ 230.Kth Smallest Element in a BST/solution.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-05-03
+Last_modify:    2016-05-03
+******************************************
+'''
+
+'''
+Given a binary search tree,
+write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often
+and you need to find the kth smallest frequently?
+How would you optimize the kthSmallest routine?
+
+Hint:
+
+Try to utilize the property of a BST.
+What if you could modify the BST node's structure?
+The optimal runtime complexity is O(height of BST).
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        if not root:
+            return None
+        stack = []
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            if k == 0:
+                return cur.val
+            cur = cur.right
+        return None
+
