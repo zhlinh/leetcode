ea588fb91444ed684c57dc88a553c0223848c49d
zhlinh <zhlinhng@gmail.com>
2016-05-07 05:59:04 +0000

    add 264.Ugly Number II

---
+++ 264.Ugly Number II/solution.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-05-07
+Last_modify:    2016-05-07
+******************************************
+'''
+
+'''
+Write a program to find the n-th ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of
+the first 10 ugly numbers.
+
+Note that 1 is typically treated as an ugly number.
+
+Hint:
+
+1. The naive approach is to call isUgly for every number
+until you reach the n-th one.  Most numbers are not ugly.
+Try to focus your effort on generating only the ugly ones.
+
+2. An ugly number must be multiplied by either 2, 3, or 5 from
+a smaller ugly number.
+
+3. The key is how to maintain the order of the ugly numbers.
+Try a similar approach of merging from three sorted lists: L1, L2, and L3.
+
+4. Assume you have Uk, the kth ugly number.
+Then Uk+1 must be Min(L1 * 2, L2 * 3, L3 * 5).
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+'''
+
+from collections import deque
+
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        q1, q2, q3 = [], [], []
+        t1, t2, t3 = 0, 0, 0
+        cur = 1
+        for i in range(n - 1):
+            q1.append(cur * 2)
+            q2.append(cur * 3)
+            q3.append(cur * 5)
+            cur = min(q1[t1], q2[t2], q3[t3])
+            if cur == q1[t1]:
+                t1 += 1
+            if cur == q2[t2]:
+                t2 += 1
+            if cur == q3[t3]:
+                t3 += 1
+        return cur
---
+++ 264.Ugly Number II/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+num = 10
+sol = Solution()
+res = sol.nthUglyNumber(num)
+print(res)
