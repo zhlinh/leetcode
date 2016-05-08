d2ef702613876f792cc906847c2872261e0c0c21
zhlinh <zhlinhng@gmail.com>
2016-05-08 07:42:07 +0000

    add 274.H-Index

---
+++ 274.H-Index/README.md
@@ -0,0 +1,10 @@
+第一版就是先排序，然后按定义找。
+
+从h的最大值n开始找起，n为citations数组的长度。
+
+submit的结果为:
+```
+81 / 81 test cases passed.
+Status: Accepted
+Runtime: 48 ms
+```
---
+++ 274.H-Index/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+citations = [3, 0, 6, 1, 5]
+sol = Solution()
+res = sol.hIndex(citations)
+print(res)
