38cf9ee02876e991dbfa61aa266068d3ddeadfab
zhlinh <zhlinhng@gmail.com>
2016-05-09 06:14:31 +0000

    add 275.H-Index II

---
+++ 275.H-Index II/solution.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-05-09
+Last_modify:    2016-05-09
+******************************************
+'''
+
+'''
+Follow up for H-Index: What if the citations array is sorted in ascending order? Could you optimize your algorithm?
+
+Hint:
+
+Expected runtime complexity is in O(log n) and the input is sorted.
+'''
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        l, r = 0, n
+        while l < r:
+            mid = l + (r - l) // 2
+            if citations[mid] >= n - mid:
+                r = mid
+            else:
+                l = mid + 1
+        return n - r
