257a1db2b8b1967a80a2be54c43a4198037e0e6c
zhlinh <zhlinhng@gmail.com>
2016-05-10 01:28:14 +0000

    add 283.Move Zeroes

---
+++ 283.Move Zeroes/README.md
@@ -0,0 +1,10 @@
+就利用一个index来记录当前非0的个数。然后再把之后的补0。
+
+时间复杂度为O(n)。
+
+submit的结果为:
+```
+21 / 21 test cases passed.
+Status: Accepted
+Runtime: 72 ms
+```
---
+++ 283.Move Zeroes/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+nums = [0, 1, 0, 3, 12]
+sol = Solution()
+sol.moveZeroes(nums)
+print(nums)
