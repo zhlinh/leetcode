13ff07ba9365f8728a91f5ba379427db975d276d
zhlinh <zhlinhng@gmail.com>
2016-05-12 16:08:37 +0000

    add 306.Additive Number

---
+++ 306.Additive Number/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+#  num = "1991001991"
+#  num = "113"
+num = "0235813"
+sol = Solution()
+res = sol.isAdditiveNumber(num)
+print(res)
