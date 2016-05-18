1ae68754219413a33ccd9a25e74e4e4f262a1218
zhlinh <zhlinhng@gmail.com>
2016-05-18 08:48:30 +0000

    recursive solution

--- 316. Remove Duplicate Letters/solution.py
+++ 316. Remove Duplicate Letters/solution.py
@@ -33,32 +33,18 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        n = len(s)
-        letters = [[] for i in range(26)]
-        countChar = 0
-        res = ""
-        for i in range(n):
+        if not s:
+            return s
+        count = [0] * 26
+        pos = 0
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        for i in range(len(s)):
+            if s[i] < s[pos]:
+                pos = i
             index = ord(s[i]) - ord('a')
-            if not letters[index]:
-                countChar += 1
-            letters[index].append(i)
-        while countChar > 0:
-            minLargest = 2 ** 31 - 1
-            chosenPos = 0
-            for li in letters:
-                if li and li[-1] < minLargest:
-                    minLargest = li[-1]
-            for i in range(len(letters)):
-                if letters[i] and letters[i][0] <= minLargest:
-                    #  print(i)
-                    res += chr(i + ord('a'))
-                    chosenPos = letters[i][0]
-                    letters[i] = []
-                    break
-            if countChar > 0:
-                for li in letters:
-                    while li and li[0] < chosenPos:
-                        li.pop(0)
-            countChar -= 1
-        return res
+            count[index] -= 1
+            if count[index] == 0:
+                break
+        return s[pos] + self.removeDuplicateLetters(s[pos+1:].replace(s[pos], ""))
 
