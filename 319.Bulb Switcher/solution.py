099fa1cf5f113ab724c266fb4eae861ace522b7a
zhlinh <zhlinhng@gmail.com>
2016-05-18 13:06:42 +0000

    add 319.Bulb Switcher

---
+++ 319.Bulb Switcher/solution.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-05-18
+Last_modify:    2016-05-18
+******************************************
+'''
+
+'''
+There are n bulbs that are initially off.
+You first turn on all the bulbs.
+Then, you turn off every second bulb.
+On the third round, you toggle every third bulb
+(turning on if it's off or turning off if it's on).
+For the i-th round, you toggle every i bulb.
+For the nth round, you only toggle the last bulb.
+Find how many bulbs are on after n rounds.
+
+Example:
+
+Given n = 3.
+
+At first, the three bulbs are [off, off, off].
+After first round, the three bulbs are [on, on, on].
+After second round, the three bulbs are [on, off, on].
+After third round, the three bulbs are [on, off, off].
+
+So you should return 1, because there is only one bulb is on.
+Subscribe to see which companies asked this question
+'''
+
+import math
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(math.sqrt(n))
