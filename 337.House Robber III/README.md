e2f17b841c3a7bcbf1caaaa66cea097e5474d1ed
zhlinh <zhlinhng@gmail.com>
2016-05-23 07:57:32 +0000

    add 337.House Robber III

---
+++ 337.House Robber III/README.md
@@ -0,0 +1,40 @@
+已经想到了用DP来解。每一个节点分为include和exclude两种情况，然后保留着两个的最大值。
+
+但问题是我想的是用BFS来至顶向下来求每一层的每个元素的两个dp值。
+
+可以以此压到另一个queue中，与维护BFS的queue保持一致就好。
+
+然后再在最后没有子节点时将两个dp的max值加到res中，这样虽然也可行但还是不够优雅。
+
+这种情况明显是要自底向上求dp值，然后在根节点处取包含和不包含当前节点的max。
+
+用DFS递归的方法，返回两个dp值，实现自底向上返回，这样就不用分散地求和。
+
+还有纯粹的DFS或改进加入cache的DFS，虽然会很慢，但在初始阶段很容易理解include和exclude的思路。
+
+无cache的纯粹DFS：
+
+```
+def rob(root):
+    if not root:
+        return 0
+    return max(robInclude(root), robExclude(root))
+
+def robInclude(node):
+    if not node:
+        return 0
+    return robExclude(node.left) + robExclude(node.right) + node.val
+
+def robExclude(node):
+    if not node:
+        return 0
+    return rob(node.left) + rob(node.right)
+```
+
+
+sumbmit的结果为:
+```
+134 / 134 test cases passed.
+Status: Accepted
+Runtime: 1160 ms
+```
---
+++ 337.House Robber III/solution.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+*****************************************
+Author:         zhlinh
+Email:          zhlinhng@gmail.com
+Version:        0.0.1
+Created Time:   2016-05-23
+Last_modify:    2016-05-23
+******************************************
+'''
+
+'''
+The thief has found himself a new place for his thievery again.
+There is only one entrance to this area, called the "root."
+Besides the root, each house has one and only one parent house.
+After a tour, the smart thief realized that "all houses in this
+place forms a binary tree". It will automatically contact the police
+if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight
+without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+Credits:
+Special thanks to @dietpepsi for adding this problem and creating all test cases.
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = self.helper(root)
+        return max(res[0], res[1])
+
+    def helper(self, node):
+        if not node:
+            return [0] * 2
+        left = self.helper(node.left)
+        right = self.helper(node.right)
+        return [node.val + left[1] + right[1], max(left[0], \
+                left[1]) + max(right[0], right[1])]
+
