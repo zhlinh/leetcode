b2f4d9a0e1310d6980ac76d8f4c3334733e29008
zhlinh <zhlinhng@gmail.com>
2016-05-23 03:47:47 +0000

    add 336.Palindrome Pairs

---
+++ 336.Palindrome Pairs/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+#  words = ["bat", "tab", "cat"]
+#  words = ["bat", "tab", "cat", ""]
+#  words = ["abcd", "dcba", "lls", "s", "sssll"]
+words = ["a", ""]
+sol = Solution()
+res = sol.palindromePairs(words)
+print(res)
